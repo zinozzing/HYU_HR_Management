b69718f7dd6918d30623b5ee4219c91c1baf43be
zinozzing <rlawlsgh1111@naver.com>
2022-09-18 18:51:53 +0900

    mod...

--- src/main/java/org/hyhr/web/HelloController.java
+++ src/main/java/org/hyhr/web/HelloController.java
@@ -1,9 +1,10 @@
 package org.hyhr.web;
-
 import org.hyhr.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class HelloController {
@@ -15,5 +16,6 @@ public class HelloController {
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
         return new HelloResponseDto(name, amount);
+
     }
 }
