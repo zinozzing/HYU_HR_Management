8fbe6eecf8475a1d339ba75e72315e259b5aae71
SONGJAEHEON <spes2you@gmail.com>
2022-09-20 19:08:10 +0900

    20220920 19:00 backend relocation

--- backup/src/main/java/org/hyhr/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package org.hyhr.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
--- backup/src/main/java/org/hyhr/domain/users/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package org.hyhr.domain.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-    Optional<Users> findByEmail(String email);
-}
