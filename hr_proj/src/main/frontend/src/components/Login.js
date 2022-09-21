16939082a7f81025de06d3f081f851ed1d784d8b
zinozzing <rlawlsgh1111@naver.com>
2022-09-21 13:10:28 +0900

    Confirm...

--- hr_proj/src/main/frontend/src/components/Login.js
+++ hr_proj/src/main/frontend/src/components/Login.js
@@ -4,7 +4,6 @@ import "./Login.css";
 
 export default function Login({ setToken }) {
   // const [username, setUserName] = useState();
-
   // const [password, setPassword] = useState();
 
   return (
